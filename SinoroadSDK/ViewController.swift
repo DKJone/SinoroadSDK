835defa788d80a725852312bca6f26cc60b60322
DKJone <1136165636@qq.com>
2017-11-29 15:52:52 +0800

    version 0.0.1

--- SinoroadSDK/ViewController.swift
+++ SinoroadSDK/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         showLoadingHud("sssss", in: self.view)
+        //
     }
 
     override func didReceiveMemoryWarning() {
